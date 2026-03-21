a2f4597a85d87e6841e5963e8954bff7e37f4de6
Rohan Perera <ropereralk@gmail.com>
2026-03-22 01:26:21 +0530

    Added: routing

--- src/app/page/user/page.tsx
+++ src/app/page/user/page.tsx
@@ -1,3 +1,60 @@
+"use client";
+
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+
 export default function UserPage() {
-  return <h1>User</h1>;
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    // wire to API / Redux when ready
+  }
+
+  return (
+    <Stack spacing={3} maxWidth={480}>
+      <Typography variant="h5" component="h1">
+        User
+      </Typography>
+
+      <Stack
+        component="form"
+        spacing={2.5}
+        onSubmit={handleSubmit}
+        noValidate
+      >
+        <TextField
+          label="Name"
+          name="name"
+          fullWidth
+          required
+          autoComplete="name"
+        />
+        <TextField
+          label="Email"
+          name="email"
+          type="email"
+          fullWidth
+          required
+          autoComplete="email"
+        />
+        <TextField
+          label="Password"
+          name="password"
+          type="password"
+          fullWidth
+          required
+          autoComplete="new-password"
+        />
+        <Stack direction="row" spacing={2} sx={{ alignSelf: "flex-start" }}>
+          <Button type="reset" variant="outlined">
+            Clear
+          </Button>
+          <Button type="submit" variant="contained">
+            Submit
+          </Button>
+        </Stack>
+      </Stack>
+    </Stack>
+  );
 }
